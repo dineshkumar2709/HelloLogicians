495492b5af34b92a01225715b534c1d793dd76e4
Dineshkumar Muthalagan <dmuthalagan@liad.com>
2020-12-03 11:11:17 +0530

    Added Greetings into Java Files

--- JenkinsJavaProgram.java
+++ JenkinsJavaProgram.java
@@ -1,6 +1,6 @@
 public class JenkinsJavaProgram{
 public static void main(String arg[]){
 System.out.println("Hello Logicians");
-
+System.out.println("Hello All, Good Morning Have a Nice Day");
 }
 }
